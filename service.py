d07781092a3438b5f39ae4f67fdb3896e7dd3712
Donna Hogan <ddhogan@users.noreply.github.com>
2020-03-24 12:36:08 -0400

    [MDS-147] Use data derived from exported file without editing
    - Select only rows with permitted Status
    - Use original column headings
    - generate location by combining fields
    - Use the first sheet so it does not need to be renamed

--- service.py
+++ service.py
@@ -117,10 +117,11 @@ def get_soup(link):
 
 
 def get_tepp_worksheet():
-    s3_response_object = s3.get_object(Bucket=BUCKET, Key='tepp_prepared.xlsx')
+    s3_response_object = s3.get_object(Bucket=BUCKET, Key='tepp_export.xlsx')
     response_body = s3_response_object['Body'].read()
     workbook = openpyxl.load_workbook(BytesIO(response_body))
-    return workbook['query']
+    first_sheet = workbook.sheetnames[0]
+    return workbook[first_sheet]
 
 
 def get_tepp_headers():
@@ -147,14 +148,14 @@ def convert_date(row, date_col):
 
 def get_first_name(row):
     try:
-        return row[get_tepp_headers()['contact_name']].partition(' ')[0]
+        return row[get_tepp_headers()['Contact Name (First and Last)']].partition(' ')[0]
     except AttributeError:
         return None
 
 
 def get_last_name(row):
     try:
-        return row[get_tepp_headers()['contact_name']].partition(' ')[-1]
+        return row[get_tepp_headers()['Contact Name (First and Last)']].partition(' ')[-1]
     except AttributeError:
         return None
 
@@ -164,59 +165,60 @@ def get_tepp_contact_info(row):
     contact['firstname'] = get_first_name(row)
     contact['title'] = None
     contact['lastname'] = get_last_name(row)
-    contact['phone'] = row[get_tepp_headers()['contact_phone']]
-    contact['post'] = row[get_tepp_headers()['org_city']]
-    contact['email'] = row[get_tepp_headers()['contact_email']]
+    contact['phone'] = row[get_tepp_headers()['Contact Phone Number']]
+    contact['post'] = row[get_tepp_headers()['City (Organization)']]
+    contact['email'] = row[get_tepp_headers()['Contact Email']]
     return contact
 
 
 def get_tepp_venue_info(row):
     venue = {}
-    venue['city'] = row[get_tepp_headers()['event_city']]
-    venue['state'] = row[get_tepp_headers()['event_state']]
-    venue['location'] = row[get_tepp_headers()['event_location']]
-    venue['country'] = row[get_tepp_headers()['event_country']]
+    venue['city'] = row[get_tepp_headers()['Event Location - City']]
+    venue['state'] = row[get_tepp_headers()['Event Location - State (U.S. Only)']]
+    venue['country'] = row[get_tepp_headers()['Event Location - Country']]
+    location_array = [venue['city'], venue['state'], venue['country']]
+    venue['location'] = ", ".join([str(item) for item in location_array if item])
     return venue
 
 
 def generate_event_id(row):
     m = hashlib.sha1()
-    unique_list = [row[get_tepp_headers()['event_name']], convert_date(row, 'event_start_date'),
-                   convert_date(row, 'event_end_date'), row[get_tepp_headers()['event_location']]]
-    m.update("".join(unique_list).encode())
+    unique_list = [row[get_tepp_headers()['Event Name']],
+                   convert_date(row, 'Event Start Date'),
+                   convert_date(row, 'Event End Date'),
+                   row[get_tepp_headers()['Event Location - City']]]
+    m.update("".join([str(item) for item in unique_list if item]).encode())
     return m.hexdigest()
 
 
 def get_tepp_industry(row):
-    if row[get_tepp_headers()['primary_industry']]:
-        return [row[get_tepp_headers()['primary_industry']]]
+    if row[get_tepp_headers()['Primary Industry']]:
+        return [row[get_tepp_headers()['Primary Industry']]]
     else:
         return []
 
 
 def get_tepp_events():
-    """
-    Assemble the event object, and then append it to the list of events.
-    When reading the excel file, skip the header row, and for each cell take only the value.
-    """
     events = []
     worksheet = get_tepp_worksheet()
+    permitted_status = ["MOA Received", "Waiting on End of Show Report", "Event Completed"]
     for row in worksheet.iter_rows(min_row=2, values_only=True):
-        event = {}
-        event['eventid'] = generate_event_id(row)
-        event['industries'] = get_tepp_industry(row)
-        event['evenddt'] = convert_date(row, 'event_end_date')
-        event['url'] = row[get_tepp_headers()['org_website']]
-        event['eventtype'] = 'Trade Events Partnership Program'
-        event['evstartdt'] = convert_date(row, 'event_start_date')
-        event['registrationlink'] = row[get_tepp_headers()['registration_link']]
-        event['contacts'] = [get_tepp_contact_info(row)]
-        event['detaildesc'] = row[get_tepp_headers()['description']]
-        event['eventname'] = row[get_tepp_headers()['event_name']]
-        event['venues'] = [get_tepp_venue_info(row)]
-        event['cost'] = row[get_tepp_headers()['cost']]
-        event['registrationtitle'] = row[get_tepp_headers()['registration_title']]
-        events.append(event)
+        if (row[get_tepp_headers()['Status']] in permitted_status):
+            event = {}
+            event['eventid'] = generate_event_id(row)
+            event['industries'] = get_tepp_industry(row)
+            event['evenddt'] = convert_date(row, 'Event End Date')
+            event['url'] = row[get_tepp_headers()['Show Website']]
+            event['eventtype'] = 'Trade Events Partnership Program'
+            event['evstartdt'] = convert_date(row, 'Event Start Date')
+            event['registrationlink'] = None
+            event['contacts'] = [get_tepp_contact_info(row)]
+            event['detaildesc'] = row[get_tepp_headers()['Show Description']]
+            event['eventname'] = row[get_tepp_headers()['Event Name']]
+            event['venues'] = [get_tepp_venue_info(row)]
+            event['cost'] = None
+            event['registrationtitle'] = None
+            events.append(event)
     print("Found %i TEPP items in the spreadsheet" % len(events))
     return events
 
--- test_service.py
+++ test_service.py
@@ -34,14 +34,16 @@ def test_ita_events():
                     'title': 'Senior Commercial Officer',
                     'phone': '+258-2135-5418',
                     'post': 'Maputo'
-                }, {
+                },
+                {
                     'email': 'Daniel.Donato@trade.gov',
                     'firstname': 'Daniel',
                     'lastname': 'Donato',
                     'title': 'Commercial Specialist',
                     'phone': '',
                     'post': 'Maputo'
-                }, {
+                },
+                {
                     'email': 'Marina.Matola@trade.gov',
                     'firstname': 'Marina',
                     'lastname': 'Matola',
@@ -50,51 +52,58 @@ def test_ita_events():
                     'post': 'Maputo'
                 }
             ],
-            'venues': [{
-                'city': 'Maputo',
-                'country': 'Mozambique',
-                'state': '',
-                'location': 'Radisson Blu Hotel Maputo'
-            }],
+            'venues': [
+                {
+                    'city': 'Maputo',
+                    'country': 'Mozambique',
+                    'state': '',
+                    'location': 'Radisson Blu Hotel Maputo'
+                }
+            ],
             'industries': []
         }
         assert entries[0] == expected_entry
 
 
 def test_tepp_events(monkeypatch):
-    workbook = load_workbook('sample_tepp_prepared.xlsx')
+    workbook = load_workbook('sample_tepp.xlsx')
 
     def mockreturn():
-        return workbook['query']
+        first_sheet = workbook.sheetnames[0]
+        return workbook[first_sheet]
 
     monkeypatch.setattr(service, "get_tepp_worksheet", mockreturn)
 
     entries = service.get_tepp_events()
-    assert len(entries) == 29
+    assert len(entries) == 32
     expected_entry = {
-        'eventid': '653e54cf30c01a6c3e91d5537a43615e2bc0a9c4',
+        'eventid': '0ddc7422238d453c18f3f643e9181788caf06257',
         'industries': ['Design & Construction'],
         'evenddt': '2020-02-07',
-        'url': 'https://www.worldofconcrete.com/en/info.html',
+        'url': 'blank',
         'eventtype': 'Trade Events Partnership Program',
         'evstartdt': '2020-02-04',
         'registrationlink': None,
-        'contacts': [{
-            'firstname': 'Jackie',
-            'title': None,
-            'lastname': 'James',
-            'phone': '972-536-6379',
-            'post': 'Irving',
-            'email': 'jackie.james@informa.com'
-        }],
-        'detaildesc': 'At World of Concrete, find all the products, resources and valuable information necessary to strengthen your business. This is the commercial construction industry’s first, largest and most important annual international event for concrete and masonry professionals across the globe!',  # NOQA: E501
+        'contacts': [
+            {
+                'firstname': 'Jackie',
+                'title': None,
+                'lastname': 'James',
+                'phone': '972-536-6379',
+                'post': 'Irving',
+                'email': 'jackie.james@informa.com'
+            }
+        ],
+        'detaildesc': None,
         'eventname': 'World of Concrete 2020',
-        'venues': [{
-            'city': 'Las Vegas',
-            'state': 'NV',
-            'location': 'Las Vegas, NV',
-            'country': 'United States'
-        }],
+        'venues': [
+            {
+                'city': 'Las Vegas',
+                'state': 'NV',
+                'country': 'USA',
+                'location': 'Las Vegas, NV, USA'
+            }
+        ],
         'cost': None,
         'registrationtitle': None
     }
